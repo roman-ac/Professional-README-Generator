343365871b2123c5df9a5e6ff7e9193690aaa60d
Roman Ac <roman25xx@gmail.com>
2022-02-10 12:42:10 +1100

    generateMarkdown updated

--- index.js
+++ index.js
@@ -62,7 +62,9 @@ const questions = () => {
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,17 +19,68 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license===licenseArr[0]){
+    return `[${licenseArr[0]}](https://opensource.org/licenses/AFL-3.0)`
+  } else if (license===licenseArr[1]){
+    return `[${licenseArr[1]}](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license===licenseArr[2]){
+    return `[${licenseArr[2]}](https://opensource.org/licenses/MIT)`
+  } else if (license===licenseArr[24]){
+    return `[${licenseArr[24]}](https://opensource.org/licenses/OSL-3.0)`
+  } else {
+    return ""
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license===licenseArr[0]){
+    return `Read more about ${licenseArr[0]} here:`
+  } else if (license===licenseArr[1]){
+    return `Read more about ${licenseArr[1]} here:`
+  } else if (license===licenseArr[2]){
+    return `Read more about ${licenseArr[2]} here:`
+  } else if (license===licenseArr[3]){
+    return `Read more about ${licenseArr[3]} here:`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  * [License](#License)
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contribution-Guidelines](#Contribution-Guidelines)
+  * [Test](#Test)
+  * [Any-Questions?](#Any-Questions)
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## Contribution-Guidelines
+  [Contributor Covenant](https://www.contributor-covenant.org/)  
+  ${data.contributing}
+  ## Test
+  ${data.testing}
+  ## Any-Questions?
+  ### Reach out here: 
+  [${data.username}](https://github.com/${data.username})  
+  ${data.email}`;
 }
 
 module.exports = generateMarkdown;
