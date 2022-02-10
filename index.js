7f995d435d27c67a49030f41b4b66a1166859a99
Roman Ac <roman25xx@gmail.com>
2022-02-10 11:29:45 +1100

    const questions and renderLicenseBadge

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt ([
+        
+        {
+            name: 'license',
+            message: 'Select kind of license for this application:',
+            type: 'list',
+            choices: ["Academic Free License v3.0", "Apache license 2.0","MIT","Open Software License 3.0"]
+        },
+        {
+            name: 'title',
+            message: 'What is the title of this application?',
+            type: 'input'
+        },
+        {
+            name: 'description',
+            message: 'What does this application do?',
+            type: 'input'
+        },
+        {
+            name: 'install',
+            message: 'How do I install this application?',
+            type: 'input'
+        },
+        {
+            name: 'usage',
+            message: 'How do I use this application?',
+            type: 'input'
+        },
+        {
+            name: 'contributing',
+            message: 'How do I contribute to this application?',
+            type: 'input'
+        },
+        {
+            name: 'testing',
+            message: 'How do I test this application?',
+            type: 'input'
+        },
+        {
+            name: 'username',
+            message: 'What is your Github username?',
+            type: 'input'
+        },
+        {
+            name: 'email',
+            message: 'What is your e-mail?',
+            type: 'input'
+        },
+        {
+            name: 'fileName',
+            message: 'What do you want to call this readme?',
+            type: 'input'
+        },
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseArr = ["Academic Free License v3.0", "Apache license 2.0", "MIT", "Open Software License 3.0"]
+
+
+function renderLicenseBadge(license) {
+  if (license===licenseArr[0]){
+    return "[![License: AFL-3.0](https://img.shields.io/badge/License-AFL--3.0-lightgrey.svg)](https://opensource.org/licenses/AFL-3.0)"
+  } else if (license===licenseArr[1]){
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)]"
+  } else if (license===licenseArr[2]){
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (license===licenseArr[3]){
+    return "[![License: OSL-3.0](https://img.shields.io/badge/License-OSL--3.0-lightgrey.svg)](https://opensource.org/licenses/OSL-3.0)"
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
